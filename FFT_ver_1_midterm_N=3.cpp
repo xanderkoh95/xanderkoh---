aaa40214822134c4b56ae62e092fa2ae95fcc2cd
xanderkoh95 <37096142+xanderkoh95@users.noreply.github.com>
2018-05-08 12:45:08 +0800

    ok

--- FFT_ver_1_midterm_N=3.cpp
+++ FFT_ver_1_midterm_N=3.cpp
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <math.h>
+#include <time.h>
 
 int Fast_Fourier_Transform(double *y_re, double *y_im, double *x_re, double *x_im, int N);
 int main()
 {
 	int i;
-	double y_re[9], y_im[9], x_re[9], x_im[9];
-	for(i=0;i<9;++i)
+	int N=9;
+	double y_re[N], y_im[N], x_re[N], x_im[N];
+	for(i=0;i<N;++i)
 	{
 		x_re[i] = i+1;
 		x_im[i] = 0.0;
 	}
-	Fast_Fourier_Transform(y_re, y_im, x_re, x_im, 9);
-	for(i=0;i<9;++i)
+	Fast_Fourier_Transform(y_re, y_im, x_re, x_im, N);
+	for(i=0;i<N;++i)
 	{
 		printf("%f + %f i\n", y_re[i], y_im[i]);
 	}
@@ -22,22 +24,21 @@ int main()
 int Fast_Fourier_Transform(double *y_re, double *y_im, double *x_re, double *x_im, int N)
 {
 	if(N==3) 
-	{
-		// included w_re,w_im
-		
+	{	
 		y_re[0] = x_re[0] + x_re[1]+x_re[2];
-		y_im[0] = x_im[0] + x_im[1]+x_im[2];
-		y_re[1] = x_re[0] -0.5* x_re[1]-0.5*x_re[2]; 
-		y_im[1] = x_im[0] -(((3)^0.5)/2)* x_im[1]+(((3)^0.5)/2)*x_im[2];
-		y_re[2] = x_re[0] -0.5* x_re[1]-0.5*x_re[2]; 
-		y_im[2] = x_im[0] +(((3)^0.5)/2)* x_im[1]-(((3)^0.5)/2)*x_im[2];
+		y_im[0] = x_im[0] + x_im[1] + x_im[2];
+		y_re[1] = x_re[0] -0.5* x_re[1]+(pow(3.0,0.5)/2)* x_im[1]-0.5*x_re[2]-(pow(3.0,0.5)/2)*x_im[2]; 
+		y_im[1] = x_im[0] -0.5* x_im[1]-(pow(3.0,0.5)/2)* x_re[1]-0.5*x_im[2]+(pow(3.0,0.5)/2)*x_re[2];
+		y_re[2] = x_re[0] -0.5* x_re[1]-(pow(3.0,0.5)/2)* x_im[1]-0.5*x_re[2]+(pow(3.0,0.5)/2)*x_im[2];
+		y_im[2] = x_im[0] -0.5* x_im[1]+(pow(3.0,0.5)/2)* x_re[1]-0.5*x_im[2]-(pow(3.0,0.5)/2)*x_re[2];
 		
 	} else 
 	{
+		//N=3
 		int k;
 		double *y_30_re, *y_30_im, *y_31_re, *y_31_im,*y_32_re,*y_32_im;
 		double *x_30_re, *x_30_im, *x_31_re, *x_31_im,*x_32_re,*x_32_im;
-		double w_re, w_im, w_N_re, w_N_im, a, b, temp;
+		double w_re, w_im, w_N_re, w_N_im, a, b , c , d , temp ;
 		y_30_re = (double *) malloc( N/3 * sizeof(double));
 		y_30_im = (double *) malloc( N/3 * sizeof(double));
 		y_31_re = (double *) malloc( N/3 * sizeof(double));
@@ -52,39 +53,67 @@ int Fast_Fourier_Transform(double *y_re, double *y_im, double *x_re, double *x_i
 		x_32_im = (double *) malloc( N/3 * sizeof(double));
 	
 		for(k=0;k<N/3;++k)
-		{
-			x_even_re[k] = x_re[2*k];
-			x_even_im[k] = x_im[2*k];
-			x_odd_re[k]  = x_re[2*k+1];
-			x_odd_im[k]  = x_im[2*k+1];
+		{   //N=3
+			x_30_re[k]=x_re[3*k];
+	     	x_30_im[k]=x_im[3*k];
+			x_31_re[k]=x_re[3*k+1];
+			x_31_im[k]=x_im[3*k+1];
+			x_32_re[k]=x_re[3*k+2];
+			x_32_im[k]=x_im[3*k+2];
+			
 		}
-		Fast_Fourier_Transform(y_even_re, y_even_im, x_even_re, x_even_im, N/2);
-		Fast_Fourier_Transform(y_odd_re, y_odd_im, x_odd_re, x_odd_im, N/2);
-		// y_k = even_k + w_N^k odd_k = even_k + (a + bi)
+		//N=3
+		Fast_Fourier_Transform(y_30_re, y_30_im, x_30_re, x_30_im, N/3);
+		Fast_Fourier_Transform(y_31_re, y_31_im, x_31_re, x_31_im, N/3);
+		Fast_Fourier_Transform(y_32_re, y_32_im, x_32_re, x_32_im, N/3);
+		//N=2: y_k = even_k + w_N^k odd_k = even_k + (a + bi)
+		//N=3 : y_k=y_30[k]+ w_N^k(y_31[k])+w_N^2k(y_32[k]) 
+		//         =y_30[k]+(w_re+iw_im)*(y_31_re+iy_31_im)+(w_re+iw_im)^2*( y_32_re+iy_32_im)
 		w_N_re =  cos(2.0*M_PI/N);
 		w_N_im = -sin(2.0*M_PI/N);
-		w_re   = 1.0;
+		w_re   = 1.0;     // initial value
 		w_im   = 0.0; 
-		for(k=0;k<N/2;++k)
+		
+		
+		for(k=0;k<N/3;++k)
 		{
-			a = w_re*y_odd_re[k] - w_im*y_odd_im[k];
-			b = w_re*y_odd_im[k] + w_im*y_odd_re[k];
-			y_re[k]     = y_even_re[k] + a;
+			a = w_re*y_31_re[k] - w_im*y_31_im[k];//real part
+			b = w_re*y_31_im[k] + w_im*y_31_re[k];//img part
+			c = pow(w_re,2)*y_32_re[k]-2*w_re*w_im*y_32_im[k]-pow(w_im,2)*y_32_re[k];  //real part
+			d = pow(w_re,2)*y_32_im[k]+2*w_re*w_im*y_32_re[k]-pow(w_im,2)*y_32_im[k];  
+			
+		/*	y_re[k]     = y_even_re[k] + a;
 			y_im[k]     = y_even_im[k] + b;
 			y_re[N/2+k] = y_even_re[k] - a;
 			y_im[N/2+k] = y_even_im[k] - b;
+			
+		*/
+		y_re[k]=y_30_re[k]+a+c;
+		y_im[k]=y_30_im[k]+b+d;
+		y_re[N/3+k]=y_30_re[k]+(-0.5)*(a+c);// problem in this one 
+		y_im[N/3+k]=y_30_im[k]+(pow(3.0,0.5)/2)*(b-d);// problem in this one
+		y_re[2*N/3+k]=y_30_re[k]+(-0.25)*(a+c);// problem in this one
+		y_im[2*N/3+k]=-y_30_im[k]+pow((pow(3.0,0.5)/2),2)*(b+d);// problem in this one
+			
+		
 			temp = w_re;
 			w_re = w_re*w_N_re - w_im*w_N_im;
 			w_im = temp*w_N_im + w_im*w_N_re;
 		}
-		free(y_even_re);
-		free(x_even_re);
-		free(y_even_im);
-		free(x_even_im);
-		free(y_odd_re);
-		free(y_odd_im);
-		free(x_odd_re);
-		free(x_odd_im);
+		//N=3
+		free(y_30_re);
+		free(x_30_re);
+		free(y_30_im);
+		free(x_30_im);
+		free(y_31_re);
+		free(y_31_im);
+		free(x_31_re);
+		free(x_31_im);
+		free(y_32_re);
+		free(y_32_im);
+		free(x_32_re);
+		free(x_32_im);
+		
 	}
 
 }
--- hm 2.2.cpp
+++ hm 2.2.cpp
@@ -5,39 +5,51 @@
 int main()
 {
 	clock_t t1, t2;				// variables for computing clocks 
-	double **A, *x, *b, T1;// x=1234, a location in memory, x[0] the value at memory 1234
-	int i, j, N=10000;
-    
+	double **A, *x, *b,*c;// x=1234, a location in memory, x[0] the value at memory 1234
+	int i,j,N;
+	double T1,T2 ;
 	
-	for(N=10;N<=10;N*=2)
+	for(N=5000;N<=5000;N*=2)
 	{
 		A = (double **) malloc( N * sizeof(double*) );//A[0],A[1];A[2] unlocated  //"double" memory 
 		                                              //take N*N in RAM
 		                                              // start from A[0]
 		                                              //A[0][0],A[0][1], undefine yet 
-		A[0] = (double *) malloc( N*N*sizeof(double));
-		
-		for(i=1;i<N;++i) A[i] = A[0]+ i*N;
+		A[0] = (double *) malloc( N*N*sizeof(double));// given initial value
+		#pragma omp parallel for
+		for(i=1;i<N;++i) A[i] = A[0] + i*N;//A[1]=A[0]+N,A[2]=A[0]+2N can be parallize
+		                                //A[1]=A[0]+N ---->A[2]=A[1]+N cannot be parallize
 		
 		x = (double *) malloc( N * sizeof(double) );
 		b = (double *) malloc( N * sizeof(double) );
+		c = (double *) malloc( N * sizeof(double) );
 		
 		
-		//A[1]=A[0]+N ---->A[2]=A[1]+N cannot to parallize
-		#pragma omp parallel  // set a initial value on every thread
+		#pragma omp parallel    
+		{
 		srand(time(NULL));       //after that get the random number
-		{
-			#pragma omp parallel for	
-		for(i=0;i<N;++i)//A[1]=A[0]+N,A[2]=A[0]+2N can be parallize
-		{
-			#pragma omp parallel for 
+		#pragma omp parallel for private(i,j)
+		for(i=0;i<N;++i)
+		    {
+			
 			for(j=0;j<N;++j)
-			{
+			  {
 				A[i][j] = rand();
+		
+			   }
+	      	x[i] = rand();
 			}
-			x[i] = rand();
-		}
-		t1 = clock();
+		
+	    }
+	   // for(i=0;i<N;++i)// check the answer of matrix A
+		 //{
+		 //	for(j=0;j<N;++j){
+			 
+		 //	printf("%f\t",A[i][j]);
+		 //	}
+		//	 printf("\n");
+		 //}
+		t1 = clock(); 
 		for(i=0;i<N;++i) 
 		{
 			b[i] = 0.0;
@@ -46,19 +58,32 @@ int main()
 				b[i] += A[i][j]*x[j];
 			}
 		}
-	}
 		t2 = clock();
+		//for(i=0;i<N;++i) 
+		//{
+		//	printf("%f\n",b[i]); 
+		//}
 		T1 = (t2-t1)/(double) CLOCKS_PER_SEC;
-		for(i=0;i<N;++i)
+	    printf(" matrix time vector before parallel is :%f\n",T1);
+	    
+		t1 = clock();
+		#pragma omp parallel for private(i,j)
+		for(i=0;i<N;++i) 
 		{
-			printf("%d\n",b[i]);
-			//for(j=0;j<N;j++)
+			c[i] = 0.0;
+			
+			for(j=0;j<N;++j)
 			{
-	        //printf("%d",A[i][j]);
-		    }
-		  //printf("\n");
+				c[i] += A[i][j]*x[j];
+			}
 		}
-		
+		t2 = clock();
+		T2 = (t2-t1)/(double) CLOCKS_PER_SEC;
+	    printf(" matrix time vector after parallel is :%f\n",T2);
+		printf(" time reduce  :%f\n",T1-T2);
+		for (i=0;i<N;++i)
+		printf("%f\n",fabs(b[i]-c[i]));
+		printf("time reduce = %f  persent ",T2*100/T1);
 		free(b);
 		free(x);
 		free(A[0]);
